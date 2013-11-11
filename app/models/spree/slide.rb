1a6f77fa1b3a1c1b26ac0af79bdfa7f673225e6b
alessandro <alessandro.tegner@gmail.com>
2013-11-11 18:44:10 +0000

    mend

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -4,9 +4,7 @@ class Spree::Slide < ActiveRecord::Base
   include Spree::Core::S3Support
   supports_s3 :image
 
-  scope :published, where(:published => true)
-
-  # attr_accessible :name, :body, :link_url, :published, :image, :position, :product_id
+  scope :published, lambda { where(:published => true) }
 
   belongs_to :product
 
